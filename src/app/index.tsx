a5c59420d49e9020cb3d71ea19eb0b77513c78bc
Alvaro Galvan <alvaro@alvarogalvan.com>
2026-02-06 15:21:40 -0600

    built hero section for homepage with db loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -60,6 +60,7 @@ export default function Home() {
           maxWidth: 1000,
         }}
       >
+        {/* --- FEATURED POST --- */}
         <YStack flex={2}>
           <ContentCard
             isNew={true}
@@ -70,44 +71,23 @@ export default function Home() {
             seoDescription={data[0].seoDescription}
           ></ContentCard>
         </YStack>
+        {/* --- MOST RECENT --- */}
         <YStack
           flex={1}
           style={{
             justifyContent: "space-between",
           }}
         >
-          <ContentCard
-            variant="compact"
-            slug="test"
-            imageUrl="https://cdn.leonardo.ai/users/1b5cfba4-58d0-4084-9594-f5e2c086b961/generations/a2da4bb7-b022-4a84-a33f-bfc1196e8373/Phoenix_Impressionist_still_life_painting_three_peaches_on_whi_0.jpg"
-            type="Photograph"
-            title="Example Title"
-            seoDescription="Example description"
-          ></ContentCard>
-          <ContentCard
-            variant="compact"
-            slug="test"
-            imageUrl="https://cdn.leonardo.ai/users/1b5cfba4-58d0-4084-9594-f5e2c086b961/generations/a2da4bb7-b022-4a84-a33f-bfc1196e8373/Phoenix_Impressionist_still_life_painting_three_peaches_on_whi_0.jpg"
-            type="Photograph"
-            title="Example Title"
-            seoDescription="Example description"
-          ></ContentCard>
-          <ContentCard
-            variant="compact"
-            slug="test"
-            imageUrl="https://cdn.leonardo.ai/users/1b5cfba4-58d0-4084-9594-f5e2c086b961/generations/a2da4bb7-b022-4a84-a33f-bfc1196e8373/Phoenix_Impressionist_still_life_painting_three_peaches_on_whi_0.jpg"
-            type="Photograph"
-            title="Example Title"
-            seoDescription="Example description"
-          ></ContentCard>
-          <ContentCard
-            variant="compact"
-            slug="test"
-            imageUrl="https://cdn.leonardo.ai/users/1b5cfba4-58d0-4084-9594-f5e2c086b961/generations/a2da4bb7-b022-4a84-a33f-bfc1196e8373/Phoenix_Impressionist_still_life_painting_three_peaches_on_whi_0.jpg"
-            type="Photograph"
-            title="Example Title"
-            seoDescription="Example description"
-          ></ContentCard>
+          {data.slice(1).map((item: any) => (
+            <ContentCard
+              slug={item.slug}
+              imageUrl={item.artwork.imageUrl}
+              type={item.artwork.type}
+              title={item.title}
+              seoDescription={item.seoDescription}
+              variant="compact"
+            ></ContentCard>
+          ))}
         </YStack>
       </XStack>
     </ScrollView>
